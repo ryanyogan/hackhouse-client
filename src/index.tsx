3e0fbd3d4c6c8a4930716f1713c2e6f7fa865e04
ryan <ryanyogan@gmail.com>
2021-04-01 12:28:52 -0500

    test query, manually wired up server.fetch

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
+import { Listings } from "./sections";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>hi</div>
+    <Listings title="TinyHouse Listings" />
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/sections/Listings/Listings.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { server } from "../../lib/api";
+import { ListingsData } from "./types";
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
+
+interface Props {
+  title: string;
+}
+
+export const Listings = ({ title }: Props) => {
+  const fetchListings = async () => {
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
+    console.log(data.listings);
+  };
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <button onClick={fetchListings}>Query Listings</button>
+    </div>
+  );
+};
