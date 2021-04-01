3972bf9493566b6996b364b2ee834036a905c8c0
ryan <ryanyogan@gmail.com>
2021-04-01 12:55:53 -0500

    basic typing for query/mutations into hand-rolled fetch

--- src/sections/Listings/Listings.tsx
+++ src/sections/Listings/Listings.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { server } from "../../lib/api";
-import { ListingsData } from "./types";
+import {
+  DeleteListingData,
+  DeleteListingVariables,
+  ListingsData,
+} from "./types";
 
 const LISTINGS = `
   query Listings {
@@ -18,6 +22,14 @@ const LISTINGS = `
   }
 `;
 
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
 interface Props {
   title: string;
 }
@@ -28,10 +40,24 @@ export const Listings = ({ title }: Props) => {
     console.log(data.listings);
   };
 
+  const deleteListing = async () => {
+    const { data } = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({
+      query: DELETE_LISTING,
+      variables: {
+        id: "",
+      },
+    });
+    console.log(data.deleteListing);
+  };
+
   return (
     <div>
       <h2>{title}</h2>
       <button onClick={fetchListings}>Query Listings</button>
+      <button onClick={deleteListing}>Delete Listings</button>
     </div>
   );
 };
