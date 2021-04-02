5d8876d391563125ccd882668ee8c2a7525f3198
ryan <ryanyogan@gmail.com>
2021-04-02 10:20:24 -0500

    switched to latest @apollo/client

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import { Listings } from "./sections";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "/api",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Listings title="TinyHouse Listings" />
+    <ApolloProvider client={client}>
+      <Listings title="TinyHouse Listings" />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
