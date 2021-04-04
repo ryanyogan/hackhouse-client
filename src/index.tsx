9b24f0d486c830332b0d157377d68a1059d50ec0
ryan <ryanyogan@gmail.com>
2021-04-04 13:14:25 -0500

    basic login form component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Listings } from "./sections";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/index.css";
-import { Home, Listing, User, NotFound } from "./sections";
+import { Home, Listing, User, NotFound, LogIn, Listings } from "./sections";
+import { Layout } from "antd";
 
 const client = new ApolloClient({
   uri: "/api",
@@ -15,14 +15,17 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/host" component={Home} />
-        <Route exact path="/listing/:id" component={Listing} />
-        <Route exact path="/listings/:location?" component={Listings} />
-        <Route exact path="/user/:id" component={User} />
-        <Route component={NotFound} />
-      </Switch>
+      <Layout id="app">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={LogIn} />
+          <Route exact path="/host" component={Home} />
+          <Route exact path="/listing/:id" component={Listing} />
+          <Route exact path="/listings/:location?" component={Listings} />
+          <Route exact path="/user/:id" component={User} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </Router>
   );
 };
