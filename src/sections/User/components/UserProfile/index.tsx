c98e3fd95f31e3e2c165276ac5505d945433db66
ryan <ryanyogan@gmail.com>
2021-04-05 15:21:11 -0500

    personal account details (stripe) added

--- src/sections/User/components/UserProfile/index.tsx
+++ src/sections/User/components/UserProfile/index.tsx
@@ -1,14 +1,42 @@
 import React from "react";
-import { Avatar, Card, Divider, Typography } from "antd";
+import { Avatar, Button, Card, Divider, Typography } from "antd";
 import { User as UserData } from "../../../../lib/graphql/queries/User/__generated__/User";
 
 const { Paragraph, Text, Title } = Typography;
 
 interface Props {
   user: UserData["user"];
+  viewerIsUser: boolean;
 }
 
-export const UserProfile = ({ user }: Props) => {
+export const UserProfile = ({ user, viewerIsUser }: Props) => {
+  const authorizedUserSection = viewerIsUser ? (
+    <>
+      <Divider />
+      <div className="user-profile__details">
+        <Title level={4}>Personal Account Details</Title>
+        <Paragraph>
+          Interested in becoming a TinyHouse host? Register with your Stripe
+          account!
+        </Paragraph>
+        <Button type="primary" className="user-profile__details-cta">
+          Connect with Stripe!
+        </Button>
+        <Paragraph type="secondary">
+          TinyHouse uses{" "}
+          <a
+            href="https://stripe.com/en-US/connect"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Stripe
+          </a>{" "}
+          to help transfer your earnings in a secure and trusted manner.
+        </Paragraph>
+      </div>
+    </>
+  ) : null;
+
   return (
     <div className="user-profile">
       <Card className="user-profile__card">
@@ -25,6 +53,7 @@ export const UserProfile = ({ user }: Props) => {
             Contact: <Text strong>{user.contact}</Text>
           </Paragraph>
         </div>
+        {authorizedUserSection}
       </Card>
     </div>
   );
--- src/sections/User/index.tsx
+++ src/sections/User/index.tsx
@@ -2,20 +2,27 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import { RouteComponentProps } from "react-router";
 import { USER_QUERY } from "../../lib/graphql/queries";
-
 import {
   User as UserData,
   UserVariables,
 } from "../../lib/graphql/queries/User/__generated__/User";
 import { Col, Layout, Row } from "antd";
 import { UserProfile } from "./components";
+import { Viewer } from "../../lib/types";
 
 const { Content } = Layout;
+
+interface Props {
+  viewer: Viewer;
+}
 interface MatchParams {
   id: string;
 }
 
-export const User = ({ match }: RouteComponentProps<MatchParams>) => {
+export const User = ({
+  match,
+  viewer,
+}: Props & RouteComponentProps<MatchParams>) => {
   const { data, loading, error } = useQuery<UserData, UserVariables>(
     USER_QUERY,
     {
@@ -26,7 +33,11 @@ export const User = ({ match }: RouteComponentProps<MatchParams>) => {
   );
 
   const user = data?.user ?? null;
-  const userProfileElement = user ? <UserProfile user={user} /> : null;
+  const viewerIsUser = viewer.id === match.params.id;
+
+  const userProfileElement = user ? (
+    <UserProfile user={user} viewerIsUser={viewerIsUser} />
+  ) : null;
 
   return (
     <Content className="user">
