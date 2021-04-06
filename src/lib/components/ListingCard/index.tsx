5ff1901a81b7824f0e979b82c05bf77045189e3a
ryan <ryanyogan@gmail.com>
2021-04-06 13:41:13 -0500

    format pricing

--- src/lib/components/ListingCard/index.tsx
+++ src/lib/components/ListingCard/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Card, Icon, Typography } from "antd";
+import { Link } from "react-router-dom";
+import { formatListingPrice, iconColor } from "../../utils";
 
 const { Title, Text } = Typography;
 
@@ -15,36 +17,38 @@ interface Props {
 }
 
 export const ListingCard = ({ listing }: Props) => {
-  const { title, image, address, price, numOfGuests } = listing;
+  const { id, title, image, address, price, numOfGuests } = listing;
 
   return (
-    <Card
-      hoverable
-      cover={
-        <div
-          style={{ backgroundImage: `url(${image})` }}
-          className="listing-card__cover-img"
-        />
-      }
-    >
-      <div className="listing-card__details">
-        <div className="listing-card__description">
-          <Title level={4} className="listing-card__price">
-            {price}
-            <span>/day</span>
-          </Title>
-          <Text strong ellipsis className="listing-card__title">
-            {title}
-          </Text>
-          <Text ellipsis className="listing-card__address">
-            {address}
-          </Text>
+    <Link to={`/listing/${id}`}>
+      <Card
+        hoverable
+        cover={
+          <div
+            style={{ backgroundImage: `url(${image})` }}
+            className="listing-card__cover-img"
+          />
+        }
+      >
+        <div className="listing-card__details">
+          <div className="listing-card__description">
+            <Title level={4} className="listing-card__price">
+              {formatListingPrice(price)}
+              <span>/day</span>
+            </Title>
+            <Text strong ellipsis className="listing-card__title">
+              {title}
+            </Text>
+            <Text ellipsis className="listing-card__address">
+              {address}
+            </Text>
+          </div>
+          <div className="listing-card__dimensions listing-card__dimensions--guests">
+            <Icon type="user" style={{ color: iconColor }} />
+            <Text>{numOfGuests} guests</Text>
+          </div>
         </div>
-        <div className="listing-card__dimensions listing-card__dimensions--guests">
-          <Icon type="user" />
-          <Text>{numOfGuests} guests</Text>
-        </div>
-      </div>
-    </Card>
+      </Card>
+    </Link>
   );
 };
