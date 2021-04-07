e0daf9736f5710064a92831fa2a8d50533952040
ryan <ryanyogan@gmail.com>
2021-04-07 13:11:08 -0500

    slight home bg image

--- src/sections/Home/index.tsx
+++ src/sections/Home/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Layout } from "antd";
 import { HomeHero } from "./components";
+import mapBackground from "./assets/map-background.jpeg";
 
 const { Content } = Layout;
 
 export const Home = () => {
   return (
-    <Content className="home">
+    <Content
+      className="home"
+      style={{ backgroundImage: `url(${mapBackground})` }}
+    >
       <HomeHero />
     </Content>
   );
