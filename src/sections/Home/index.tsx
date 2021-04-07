9660c935b3d3e76bf45e6688de9146f0348944ea
ryan <ryanyogan@gmail.com>
2021-04-07 13:18:20 -0500

    cleaned up the search method a tad

--- src/sections/Home/index.tsx
+++ src/sections/Home/index.tsx
@@ -2,16 +2,26 @@ import React from "react";
 import { Layout } from "antd";
 import { HomeHero } from "./components";
 import mapBackground from "./assets/map-background.jpeg";
+import { RouteComponentProps } from "react-router";
+import { displayErrorMessage } from "../../lib/utils";
 
 const { Content } = Layout;
 
-export const Home = () => {
+export const Home = ({ history }: RouteComponentProps) => {
+  const onSearch = (value: string) => {
+    const trimmedValue = value.trim();
+    if (trimmedValue.length <= 0) {
+      return displayErrorMessage("Please enter a valid search.");
+    }
+    history.push(`/listings/${value}`);
+  };
+
   return (
     <Content
       className="home"
       style={{ backgroundImage: `url(${mapBackground})` }}
     >
-      <HomeHero />
+      <HomeHero onSearch={onSearch} />
     </Content>
   );
 };
