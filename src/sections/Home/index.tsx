3adf52496c319326e139b52843b107d9be09d956
ryan <ryanyogan@gmail.com>
2021-04-07 13:52:19 -0500

    home page compelte

--- src/sections/Home/index.tsx
+++ src/sections/Home/index.tsx
@@ -1,24 +1,62 @@
 import React from "react";
 import { Col, Layout, Row, Typography } from "antd";
-import { HomeHero } from "./components";
+import { HomeHero, HomeListings, HomeListingsSkeleton } from "./components";
 import { RouteComponentProps } from "react-router";
 import { displayErrorMessage } from "../../lib/utils";
 import { Link } from "react-router-dom";
+import {
+  Listings as ListingsData,
+  ListingsVariables,
+} from "../../lib/graphql/queries/Listings/__generated__/Listings";
 
 import mapBackground from "./assets/map-background.jpeg";
 import sanFransiscoImage from "./assets/san-fransisco.jpeg";
 import cancunImage from "./assets/cancun.jpeg";
+import { useQuery } from "@apollo/client";
+import { LISTINGS_QUERY } from "../../lib/graphql/queries";
+import { ListingsFilter } from "../../lib/graphql/globalTypes";
 
 const { Content } = Layout;
 const { Title, Paragraph } = Typography;
 
+const PAGE_LIMIT = 4;
+const PAGE_NUMBER = 1;
+
 export const Home = ({ history }: RouteComponentProps) => {
+  const { loading, data } = useQuery<ListingsData, ListingsVariables>(
+    LISTINGS_QUERY,
+    {
+      variables: {
+        filter: ListingsFilter.PRICE_HIGH_TO_LOW,
+        limit: PAGE_LIMIT,
+        page: PAGE_NUMBER,
+      },
+    }
+  );
+
   const onSearch = (value: string) => {
     const trimmedValue = value.trim();
     if (trimmedValue.length <= 0) {
       return displayErrorMessage("Please enter a valid search.");
     }
     history.push(`/listings/${value}`);
+  };
+
+  const renderListingsSection = () => {
+    if (loading) {
+      return <HomeListingsSkeleton />;
+    }
+
+    if (data) {
+      return (
+        <HomeListings
+          title="Premium Listings"
+          listings={data.listings.result}
+        />
+      );
+    }
+
+    return null;
   };
 
   return (
@@ -44,6 +82,8 @@ export const Home = ({ history }: RouteComponentProps) => {
           Popular listings in the United States
         </Link>
       </div>
+
+      {renderListingsSection()}
 
       <div className="home__listings">
         <Title level={4} className="home__listings-title">
