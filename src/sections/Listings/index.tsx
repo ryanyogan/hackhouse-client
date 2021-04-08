0fe8b868bc9d45d528557f9bb2ddb4b45bf9855b
ryan <ryanyogan@gmail.com>
2021-04-08 10:49:50 -0500

    listings page can geo search, filter, and paginate

---
+++ src/sections/Listings/index.tsx
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { Affix, Layout, List, Typography } from "antd";
+import { RouteComponentProps } from "react-router";
+import { ErrorBanner, ListingCard } from "../../lib/components";
+import { ListingsFilter } from "../../lib/graphql/globalTypes";
+import { LISTINGS_QUERY } from "../../lib/graphql/queries";
+import {
+  Listings as ListingsData,
+  ListingsVariables,
+} from "../../lib/graphql/queries/Listings/__generated__/Listings";
+import { Link } from "react-router-dom";
+import {
+  ListingsFilters,
+  ListingsPagination,
+  ListingsSkeleton,
+} from "./components";
+
+const { Content } = Layout;
+const { Title, Paragraph, Text } = Typography;
+const PAGE_LIMIT = 8;
+
+interface MatchParams {
+  location: string;
+}
+
+export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
+  const [filter, setFilter] = useState(ListingsFilter.PRICE_LOW_TO_HIGH);
+  const [page, setPage] = useState(1);
+
+  const { loading, data, error } = useQuery<ListingsData, ListingsVariables>(
+    LISTINGS_QUERY,
+    {
+      variables: {
+        location: match.params.location,
+        filter,
+        limit: PAGE_LIMIT,
+        page,
+      },
+    }
+  );
+
+  if (loading) {
+    return (
+      <Content className="listings">
+        <ListingsSkeleton />
+      </Content>
+    );
+  }
+
+  if (error) {
+    return (
+      <Content className="listings">
+        <ErrorBanner
+          description={`
+            We either could not find anything matching your search or you have encountered an error.
+            If you are searching for a unique location, try searching again with more common keywords.
+          `}
+        />
+        <ListingsSkeleton />
+      </Content>
+    );
+  }
+  const listings = data ? data.listings : null;
+  const listingsRegion = listings ? listings.region : null;
+
+  const listingsSectionElement =
+    listings && listings.result.length ? (
+      <div>
+        <Affix offsetTop={64}>
+          <>
+            <ListingsPagination
+              total={listings.total}
+              page={page}
+              limit={PAGE_LIMIT}
+              setPage={setPage}
+            />
+            <ListingsFilters filter={filter} setFilter={setFilter} />
+          </>
+        </Affix>
+        <List
+          grid={{
+            gutter: 8,
+            xs: 1,
+            md: 2,
+            lg: 4,
+          }}
+          dataSource={listings.result}
+          renderItem={(listing) => (
+            <List.Item>
+              <ListingCard listing={listing} />
+            </List.Item>
+          )}
+        />
+      </div>
+    ) : (
+      <div>
+        <Paragraph>
+          It appears that no listings have yet been created for{" "}
+          <Text mark>"{listingsRegion}"</Text>
+        </Paragraph>
+        <Paragraph>
+          Be the first person to create a{" "}
+          <Link to="/host">listing in this area</Link>!
+        </Paragraph>
+      </div>
+    );
+
+  const listingsRegionElement = listingsRegion ? (
+    <Title level={3} className="listings__title">
+      Results for "{listingsRegion}"
+    </Title>
+  ) : null;
+
+  return (
+    <Content className="listings">
+      {listingsRegionElement}
+      {listingsSectionElement}
+    </Content>
+  );
+};
