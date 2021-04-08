689ee94d7be7270597072a1ed0c8fe16d3ca0460
ryan <ryanyogan@gmail.com>
2021-04-08 10:08:11 -0500

    basic listings page

--- src/sections/Listings/Listings.tsx
+++ src/sections/Listings/Listings.tsx
@@ -1,5 +1,44 @@
+import { useQuery } from "@apollo/client";
+import { Layout, List } from "antd";
 import React from "react";
+import { ListingCard } from "../../lib/components";
+import { ListingsFilter } from "../../lib/graphql/globalTypes";
+import { LISTINGS_QUERY } from "../../lib/graphql/queries";
+import {
+  Listings as ListingsData,
+  ListingsVariables,
+} from "../../lib/graphql/queries/Listings/__generated__/Listings";
+
+const { Content } = Layout;
+const PAGE_LIMIT = 8;
 
 export const Listings = () => {
-  return <h2>Listings</h2>;
+  const { data } = useQuery<ListingsData, ListingsVariables>(LISTINGS_QUERY, {
+    variables: {
+      filter: ListingsFilter.PRICE_LOW_TO_HIGH,
+      limit: PAGE_LIMIT,
+      page: 1,
+    },
+  });
+
+  const listings = data ? data.listings : null;
+
+  const listingsSectionElement = listings ? (
+    <List
+      grid={{
+        gutter: 8,
+        xs: 1,
+        md: 2,
+        lg: 4,
+      }}
+      dataSource={listings.result}
+      renderItem={(listing) => (
+        <List.Item>
+          <ListingCard listing={listing} />
+        </List.Item>
+      )}
+    />
+  ) : null;
+
+  return <Content className="listings">{listingsSectionElement}</Content>;
 };
